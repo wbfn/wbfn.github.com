60b9a6c72a3cba3dc10e648bd2ae1303395eda54
Ariel Nunez <ingenieroariel@gmail.com>
2012-01-17 22:05:55 -0500

    Added placeholders for sections

---
+++ application.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+  $('.add-on :checkbox').click(function () {
+    if ($(this).attr('checked')) {
+      $(this).parents('.add-on').addClass('active')
+    } else {
+      $(this).parents('.add-on').removeClass('active')
+    }
+  })
+
+
+  // POSITION STATIC TWIPSIES
+  // ========================
+
+  $(window).bind( 'load resize', function () {
+    $(".twipsies a").each(function () {
+       $(this)
+        .twipsy({
+          live: false
+        , placement: $(this).attr('title')
+        , trigger: 'manual'
+        , offset: 2
+        })
+        .twipsy('show')
+      })
+  })
+});
